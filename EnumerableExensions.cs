9c1c40fbd69be0db574bb4fa3903ca93a5c63ebe
Kahou <creator.kahou@gmail.com>
2016-09-21 22:39:27 +0800

    Add EnumerableExensions

---
+++ EnumerableExensions.cs
@@ -0,0 +1,20 @@
+﻿namespace System.Linq
+{
+    public static class EnumerableExensions
+    {
+        /// <summary>
+        /// Determines whether a sequence contains any elements.
+        /// </summary>
+        /// <returns>true if the source sequence contains any elements; otherwise, false.</returns>
+        static public bool HasElement<T>(this Collections.Generic.IEnumerable<T> enumeralbe)
+        {
+            if (enumeralbe == null || !enumeralbe.Any())
+            {
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
+
